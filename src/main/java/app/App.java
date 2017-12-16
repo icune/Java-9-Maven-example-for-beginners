247c1281a414364a7b92acb42c89d4a1fb843a85
iCune <icune@ya.ru>
2017-12-16 03:55:21 +0300

    Tutorial

---
+++ src/main/java/app/App.java
@@ -0,0 +1,12 @@
+package app;
+
+import plot.Plot;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Plot demo = new Plot("XY Series Demo");
+	    demo.doit();
+    }
+}
